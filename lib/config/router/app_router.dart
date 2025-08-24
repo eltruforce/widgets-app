0164636f9186251cbdda89688e79107b1a89687e
eltruforce <lordace550@gmail.com>
2025-08-24 19:56:30 +1200

    feat: add routes with names

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,11 +4,20 @@ import 'package:widgets_app/presentation/screens/screens.dart';
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+    ),
     GoRoute(
       path: '/buttons',
+      name: ButtonsScreen.name,
       builder: (context, state) => const ButtonsScreen(),
     ),
-    GoRoute(path: '/cards', builder: (context, state) => const CardsScreen()),
+    GoRoute(
+      path: '/cards',
+      name: CardsScreen.name,
+      builder: (context, state) => const CardsScreen(),
+    ),
   ],
 );
