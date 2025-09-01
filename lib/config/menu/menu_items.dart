11baa34832fed799c325c87a0d9ca6c9577113ba
eltruforce <lordace550@gmail.com>
2025-09-01 21:45:43 +1200

    feat: add tutorial screen

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -51,4 +51,10 @@ const appMenuItems = <MenuItem>[
     link: '/ui-controls',
     icon: Icons.car_rental_outlined,
   ),
+  MenuItem(
+    title: 'Introduction to the application',
+    subTitle: 'Small introductory tutorial',
+    link: '/tutorial',
+    icon: Icons.accessible_rounded,
+  ),
 ];
